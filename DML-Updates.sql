14510780e0b828d800b674e573e220f7197bb219
LariStar <109998421+LariStar@users.noreply.github.com>
2022-09-18 11:26:41 -0300

    arquivo atualizado, considerar este

--- DML-Updates.sql
+++ DML-Updates.sql
@@ -1,4 +1,9 @@
 use loja_virtual;
 
-select * from produtos;
+update produtos set valor = 59.90 where idproduto = 2;
+update produtos set cor = 'amarela' where idproduto = 5;
+update produtos set tamanho = 'GG' where idproduto = 1;
+update produtos set estoque = 20 where idproduto = 15;
+update produtos set valor = 69.90 where idproduto = 9;
 
+select * from produtos;
