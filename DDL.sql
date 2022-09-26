b507e47158b6abc5ab39fbe26580a5c9a5be937e
LariStar <109998421+LariStar@users.noreply.github.com>
2022-09-26 19:11:07 -0300

    arquivo criação das tabelas - todo 6

--- DDL.sql
+++ DDL.sql
@@ -2,14 +2,35 @@ create database if not exists loja_virtual;
 
 use loja_virtual;
 
-select * from produtos;
+CREATE TABLE IF NOT EXISTS produtos (
+    idproduto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    categoria VARCHAR(64) NOT NULL,
+    tipo VARCHAR(64) NOT NULL,
+    tamanho VARCHAR(64) NOT NULL,
+    cor VARCHAR(64) NOT NULL,
+    valor DECIMAL(10 , 2 ) NOT NULL,
+    estoque VARCHAR(64) NOT NULL
+);
 
-create table if not exists produtos(
-idproduto int not null auto_increment primary key,
-categoria varchar(64) not null,
-tipo varchar(64) not null,
-tamanho varchar(64) not null,
-cor varchar(64) not null,
-valor decimal(10,2) not null,
-estoque varchar(64) not null
+CREATE TABLE IF NOT EXISTS clientes (
+    idclientes INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cpf BIGINT(11) NOT NULL,
+    nome VARCHAR(64) NOT NULL,
+    endereco VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    telefone VARCHAR(13) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS avaliacoes (
+    idavaliacao INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    `data` date NOT NULL,
+    comentario VARCHAR(255) NOT NULL,
+    nota int NOT NULL,
+    idproduto INTEGER,
+    idclientes INTEGER,
+    FOREIGN KEY (idproduto)
+        REFERENCES produtos (idproduto),
+    FOREIGN KEY (idclientes)
+        REFERENCES clientes (idclientes)
 );
